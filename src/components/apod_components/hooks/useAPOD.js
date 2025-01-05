5ad17ab24097f476741f3b9a5b98dfe294d85fb2
Mariam Mansour <mariummansour19@gmail.com>
2025-01-05 14:25:39 +0100

    feat: Replace useEffect hook with useQuery hook for APOD query

--- src/components/apod_components/hooks/useAPOD.js
+++ src/components/apod_components/hooks/useAPOD.js
@@ -1,26 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 
+const fetchAPOD = async ({ queryKey }) => {
+    const [, url] = queryKey;
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            const apodData = await res.json();
+            return Promise.reject(`Error: ${apodData.code} - ${apodData.msg || 'Unknown error'}`);
+
+        }
+        const apodData = await res.json();
+        return apodData;
+    }
+    catch (e) {
+        return Promise.reject(e);
+    }
+}
 function useAPOD(location) {
     const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
     const params = location.state;
     const dateParam = params.date;
     const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}&concept_tags=${true}&date=${dateParam}`;
-    const fetchAPOD = async ({ queryKey }) => {
-        const [, url] = queryKey;
-        try {
-            const res = await fetch(url);
-            if (!res.ok) {
-                const apodData = await res.json();
-                return Promise.reject(`Error: ${apodData.code} - ${apodData.msg || 'Unknown error'}`);
-
-            }
-            const apodData = await res.json();
-            return apodData;
-        }
-        catch (e) {
-            return Promise.reject(e);
-        }
-    }
     const { data:apodQuery, isLoading, isError, error } = useQuery({
         queryKey: ["apod", url],
         queryFn: fetchAPOD,
