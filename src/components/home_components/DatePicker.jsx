53072701434bb65034975d02da104a1c18065444
Mariam Mansour <mariummansour19@gmail.com>
2025-01-05 15:47:38 +0100

    fix: DatePicer customI nput ref error

--- src/components/home_components/DatePicker.jsx
+++ src/components/home_components/DatePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { forwardRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import useFormat from "../apod_components/hooks/useFormat";
@@ -6,15 +6,16 @@ import useFormat from "../apod_components/hooks/useFormat";
 const IconButtonDatepicker = (props) => {
   var { setSearchQuery, date, setDate } = props
 
-  const CustomInput = ({ value, onClick }) => (
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
     <button
-    className="searchBtn datepicker-input"
+      className="searchBtn datepicker-input"
       type="button"
       onClick={onClick}
+      ref={ref}
     >
       <i className="fa-solid fa-calendar"></i>
     </button>
-  );
+  ));
 
   function onSearchQuery(date) {
     setSearchQuery(useFormat(date));
@@ -22,14 +23,14 @@ const IconButtonDatepicker = (props) => {
   }
 
   return (
-      <DatePicker
-        id="datepicker"
-        selected={date}
-        onChange={(date) => onSearchQuery(date)}
-        dateFormat="YYYY-MM-DD"
-        customInput={<CustomInput />}
-        popperPlacement="top-start"
-      />
+    <DatePicker
+      id="datepicker"
+      selected={date}
+      onChange={(date) => onSearchQuery(date)}
+      dateFormat="YYYY-MM-DD"
+      customInput={<CustomInput />}
+      popperPlacement="top-start"
+    />
   );
 };
 
