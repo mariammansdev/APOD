2bfbd665a97c4214e23cce86365682a6c5eca650
Mariam Mansour <mariummansour19@gmail.com>
2025-02-17 16:48:04 +0100

    feat: Styles enhancements

--- src/components/home_components/Body.jsx
+++ src/components/home_components/Body.jsx
@@ -4,7 +4,7 @@ function Body() {
   return (
       <div className='bodyContainer'>
         <div className='bodyContent'>
-          <h2 className="apodTitle">Explore Your Universe!</h2>
+          <h2>Explore Your Universe!</h2>
           <p className='colorChangeText' > APOD offers a daily visual feast of our universe, bringing the latest discoveries, breathtaking imagery, and educational content to the public, one picture at a time.</p>
         </div>
         <SearchContainer />
